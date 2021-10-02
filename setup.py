17497ac42fdba6626a7f71a1998682848f8f0c42
mattias <mattias.decharleroy@gmail.com>
2021-10-02 15:33:55 +0200

    Change setup file to include required packages

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import os
 from setuptools import setup, find_packages
 
-setup(name='stock-market-engine', version='0.1', packages=find_packages())
+requirements_path = './requirements.txt'
+install_requires = []
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(name='stock-market-engine', version='0.1', install_requires=install_requires, packages=find_packages())

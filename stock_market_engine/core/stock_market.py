b8d7acb1a455da99f36bfa8c6390e323164c643a
mattias <mattias.decharleroy@gmail.com>
2021-10-02 16:57:30 +0200

    Fix bug in stock market

--- stock_market_engine/core/stock_market.py
+++ stock_market_engine/core/stock_market.py
@@ -14,4 +14,4 @@ class StockMarket:
 
 	def update_ticker(self, ticker_OHLC):
 		assert(ticker_OHLC.ticker in self.tickers)
-		self.__ticker_OHLCs[ticker_OHLC.ticker] = ticker_OHLC
+		self.__ticker_OHLCs[ticker_OHLC.ticker] = ticker_OHLC.ohlc

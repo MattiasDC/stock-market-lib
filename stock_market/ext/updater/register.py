1ecfd8f9e620cb918ff149304c9a9f353c28223f
mattias <mattias.decharleroy@gmail.com>
2022-06-29 17:32:10 +0200

    Extend stock updater factory to include proxy

--- stock_market/ext/updater/register.py
+++ stock_market/ext/updater/register.py
@@ -1,9 +1,15 @@
+from .proxy_stock_updater import ProxyStockUpdater
 from .yahoo_finance_stock_updater import YahooFinanceStockUpdater
 
 
 def register_stock_updater_factories(factory):
-    return factory.register(
+    factory = factory.register(
         "yahoo",
         YahooFinanceStockUpdater.from_json,
         YahooFinanceStockUpdater.json_schema(),
+    )
+    return factory.register(
+        "proxy",
+        ProxyStockUpdater.from_json,
+        ProxyStockUpdater.json_schema(),
     )

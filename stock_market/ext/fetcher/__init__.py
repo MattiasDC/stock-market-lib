79efb6f8e1aceb1cddd0b999079984d13c8436b3
mattias <mattias.decharleroy@gmail.com>
2022-06-30 19:59:34 +0200

    Keep stock_updater_factories i.s.o ohlc fetcher factories

--- stock_market/ext/fetcher/__init__.py
+++ stock_market/ext/fetcher/__init__.py
@@ -1,3 +1,3 @@
 from .proxy_ohlc_fetcher import ProxyOHLCFetcher  # noqa
-from .register import register_ohlc_fetcher_factories  # noqa
+from .register import register_stock_updater_factories  # noqa
 from .yahoo_ohlc_fetcher import YahooOHLCFetcher  # noqa
--- stock_market/ext/fetcher/register.py
+++ stock_market/ext/fetcher/register.py
@@ -1,15 +1,17 @@
+from stock_market.core import StockUpdater
+
 from .proxy_ohlc_fetcher import ProxyOHLCFetcher
 from .yahoo_ohlc_fetcher import YahooOHLCFetcher
 
 
-def register_ohlc_fetcher_factories(factory):
+def register_stock_updater_factories(factory):
     factory = factory.register(
         "yahoo",
-        YahooOHLCFetcher.from_json,
+        lambda config: StockUpdater(YahooOHLCFetcher.from_json(config)),
         YahooOHLCFetcher.json_schema(),
     )
     return factory.register(
         "proxy",
-        ProxyOHLCFetcher.from_json,
+        lambda config: StockUpdater(ProxyOHLCFetcher.from_json(config)),
         ProxyOHLCFetcher.json_schema(),
     )
